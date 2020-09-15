57fa1235780342a162a1685ec4bac200e9ce49f0
Alan Corrales <alancorrales31@gmail.com>
2020-09-15 02:33:56 -0300

    Adds register user feature

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,50 +1,85 @@
-import React, { useReducer } from 'react'
-import AuthContext from './authContext'
-import authReducer from './authReducer'
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import AuthContext from './authContext';
+import authReducer from './authReducer';
 import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    USER_LOADED,
-    AUTH_ERROR,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT,
-    CLEAR_ERRORS
-} from '../types'
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT,
+	CLEAR_ERRORS,
+} from '../types';
 
-const AuthState = props => {
-    const initialState = {
-        token: localStorage.getItem('token'),
-        isAtuhenticated: null,
-        user: null,
-        loading: true,
-        error: null
-    }
+const AuthState = (props) => {
+	const initialState = {
+		token: localStorage.getItem('token'),
+		isAtuhenticated: null,
+		user: null,
+		loading: true,
+		error: null,
+	};
 
-    const [state, dispatch] = useReducer(authReducer, initialState)
+	const [state, dispatch] = useReducer(authReducer, initialState);
 
-    // Load user
+	// Load user
+	const loadUser = () => console.log('load user');
 
-    // Register user
+	// Register user
+	const register = async (formData) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
 
-    // Log in user
+		try {
+			const res = await axios.post('api/users', formData, config);
 
-    // Logout
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			});
+		} catch (error) {
+			dispatch({
+				type: REGISTER_FAIL,
+				payload: error.response.data.msg,
+			});
+		}
+	};
 
-    // Clear errors
+	// Log in user
+	const login = () => console.log('login');
 
-    return (
-        <AuthContext.Provider
-            value={{
-                token: state.token,
-                isAtuhenticated: state.isAtuhenticated,
-                loading: state.loading,
-                errors: state.errors,
-                user: state.user
-            }}>
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
+	// Logout
+	const logout = () => console.log('logout');
+
+	// Clear errors
+	const clearErrors = () =>
+		dispatch({
+			type: CLEAR_ERRORS,
+		});
+
+	return (
+		<AuthContext.Provider
+			value={{
+				token: state.token,
+				isAtuhenticated: state.isAtuhenticated,
+				loading: state.loading,
+				user: state.user,
+				error: state.error,
+				register,
+				loadUser,
+				login,
+				logout,
+				clearErrors,
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
+};
 
 export default AuthState;
